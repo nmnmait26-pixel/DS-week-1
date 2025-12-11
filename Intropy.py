45087dd7c7bf46b2e0796e75b94d37385f7937d7
nmnmait26-pixel <nmn.mait26@gmail.com>
2025-12-12 01:37:38 +0530

    Intro_py

--- Intropy.py
+++ Intropy.py
@@ -1,15 +1,10 @@
-# Personal Introduction Program
-# Final Version
+print("Welcome! Let's get to know you \n")
 
-# Asking user for details
 name = input("What is your name? ")
 age = input("How old are you? ")
 hobby = input("What is your favorite hobby? ")
 city = input("Which city do you live in? ")
-food = input("What is your favorite food? ")
 
-# Displaying welcome message
-print("\n🎉 Welcome", name + "! 🎉")
-print(f"You are {age} years old, live in {city}, and love {hobby}.")
-print(f"Your favorite food is {food}.")
-print("Nice to meet you! 😊")
+print("\n Welcome", name + "! ")
+print(f"You are {age} years old \nYou live in {city} \nYou love {hobby}.")
+print("Glad to have you here!")
